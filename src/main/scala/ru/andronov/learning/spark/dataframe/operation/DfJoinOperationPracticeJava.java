358609ae919a5871dcfde22cf2cf8c6b00cbe1bc
Andrei Andronov <ao.andronov@gmail.com>
2022-08-26 12:20:23 +0300

    feat: broadcast join

--- src/main/scala/ru/andronov/learning/spark/dataframe/operation/DfJoinOperationPracticeJava.java
+++ src/main/scala/ru/andronov/learning/spark/dataframe/operation/DfJoinOperationPracticeJava.java
@@ -5,7 +5,7 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import ru.andronov.learning.spark.dataframe.model.ModelCreatorJava;
 
-import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.*;
 
 public class DfJoinOperationPracticeJava {
 
@@ -19,8 +19,12 @@ public class DfJoinOperationPracticeJava {
         Dataset<Row> postDf = ModelCreatorJava.createPostDf(spark);
         Dataset<Row> userDF = ModelCreatorJava.createUserDF(spark);
 
+        System.out.println("full outer join");
         Dataset<Row> joinedDF = fullOuterJoin(postDf, userDF);
         joinedDF.show(10, false);
+
+        System.out.println("broadcast full outer join");
+        userDF.as("u").join(broadcast(postDf.as("p")), col("u.id").equalTo(col("p.userId")), "full_outer").show();
     }
 
     public static Dataset<Row> fullOuterJoin(Dataset<Row> postDf, Dataset<Row> userDF) {
